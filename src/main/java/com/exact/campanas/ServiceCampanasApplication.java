919d40a233941fc7a89c3c82f546bbefee6a8131
RonaldSantosR <rsantos@exact.com.pe>
2018-11-28 14:48:57 -0500

    Creacion de Proyecto

---
+++ src/main/java/com/exact/campanas/ServiceCampanasApplication.java
@@ -0,0 +1,12 @@
+package com.exact.campanas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceCampanasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceCampanasApplication.class, args);
+	}
+}
