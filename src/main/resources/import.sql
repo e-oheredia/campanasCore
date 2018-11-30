64cc32041f6b5a58f5bdd506acf26ee987264a93
cbaltazar7 <cbaltazar@exact.com.pe>
2018-11-30 14:46:32 -0500

    Commit - import sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,3 +2,19 @@ SET IDENTITY_INSERT [dbo].[tipo_destino] ON
 INSERT INTO [dbo].[tipo_destino]([tipo_destino_id],[nombre]) VALUES(1,'INTERNA')
 INSERT INTO [dbo].[tipo_destino]([tipo_destino_id],[nombre]) VALUES(2,'EXTERNA')
 SET IDENTITY_INSERT [dbo].[tipo_destino] OFF
+
+SET IDENTITY_INSERT [dbo].[tipo_agrupado] ON 
+INSERT INTO [dbo].[tipo_agrupado]([tipo_agrupado_id],[nombre]) VALUES(1,'POR AGENCIA')
+INSERT INTO [dbo].[tipo_agrupado]([tipo_agrupado_id],[nombre]) VALUES(2,'POR UNIDAD ORGANIZATIVA')
+INSERT INTO [dbo].[tipo_agrupado]([tipo_agrupado_id],[nombre]) VALUES(3,'POR SUCURSAL - SEDE')
+SET IDENTITY_INSERT [dbo].[tipo_agrupado] OFF
+
+SET IDENTITY_INSERT [dbo].[accion_cargo_fisico] ON 
+INSERT INTO [dbo].[accion_cargo_fisico]([accion_cargo_fisico_id],[nombre]) VALUES(1,'QUE EL PROVEEDOR LOS DESTRUYA')
+INSERT INTO [dbo].[accion_cargo_fisico]([accion_cargo_fisico_id],[nombre]) VALUES(2,'DEVOLVER A ALMACEN CENTRAL')
+SET IDENTITY_INSERT [dbo].[accion_cargo_fisico] OFF
+
+SET IDENTITY_INSERT [dbo].[accion_rezago_fisico] ON 
+INSERT INTO [dbo].[accion_rezago_fisico]([accion_rezago_fisico_id],[nombre]) VALUES(1,'QUE EL PROVEEDOR LOS DESTRUYA')
+INSERT INTO [dbo].[accion_rezago_fisico]([accion_rezago_fisico_id],[nombre]) VALUES(2,'DEVOLVER A ALMACEN CENTRAL')
+SET IDENTITY_INSERT [dbo].[accion_rezago_fisico] OFF
