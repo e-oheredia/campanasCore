1f61b4952fff64244aa2c9f63f82ec687197ac6e
e-oheredia <oheredia@exact.com.pe>
2020-02-13 13:01:15 -0500

    Implementación de Campañas en DEMO

--- src/main/java/com/exact/service/campana/SpringSecurityConfig.java
+++ src/main/java/com/exact/service/campana/SpringSecurityConfig.java
@@ -1,16 +1,12 @@
 package com.exact.service.campana;
 
-import java.util.Arrays;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.security.web.session.SessionManagementFilter;
 
 import com.exact.service.campana.auth.filter.JWTAuthorizationFilter;
 
@@ -21,18 +17,15 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.addFilter(new JWTAuthorizationFilter(authenticationManager())).csrf().disable().sessionManagement()
-		.sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().cors();
+		http
+		.addFilterBefore(corsFilter(), JWTAuthorizationFilter.class)
+		.addFilter(new JWTAuthorizationFilter(authenticationManager())).csrf().disable().sessionManagement()
+		.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}	
 	
 	@Bean
-	CorsConfigurationSource corsConfigurationSource() {
-		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(Arrays.asList("*"));
-		configuration.setAllowedMethods(Arrays.asList("GET","POST", "PUT", "DELETE"));
-		configuration.setAllowedHeaders(Arrays.asList("Authorization","Content-type", "multipart/form-data"));
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", configuration);
-		return source;
-	}
+    CorsFilter corsFilter() {
+        CorsFilter filter = new CorsFilter();
+        return filter;
+    }
 }
